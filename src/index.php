d6370487405226cab7ad1d9f9a23f4a131b166bd
netkas <netkas@intellivoid.info>
2019-06-23 16:22:17 -0400

    Minor bug fix

--- src/index.php
+++ src/index.php
@@ -5,10 +5,11 @@
     require __DIR__ . '/vendor/autoload.php';
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'CoffeeHouse' . DIRECTORY_SEPARATOR . 'CoffeeHouse.php');
 
+
     $CoffeeHouse = new CoffeeHouse();
     $telegram = new Longman\TelegramBot\Telegram(
         $CoffeeHouse->getTelegramConfiguration()['ApiKey'],
         $CoffeeHouse->getTelegramConfiguration()['BotName']
     );
-    $telegram->addCommandsPaths([__DIR__ . DIRECTORY_SEPARATOR,]);
+    $telegram->addCommandsPaths([__DIR__ . DIRECTORY_SEPARATOR . 'commands']);
     $telegram->handle();
