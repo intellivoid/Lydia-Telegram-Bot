3cd23637236c9d9654ff655511b337e87d3939b7
netkas <netkas@intellivoid.info>
2020-02-16 13:06:18 -0300

    Minor bug fixes

--- src/index.php
+++ src/index.php
@@ -2,7 +2,6 @@
 
     use acm\acm;
     use acm\Objects\Schema;
-    use CoffeeHouse\CoffeeHouse;
     use Longman\TelegramBot\Exception\TelegramException;
 
     require __DIR__ . '/vendor/autoload.php';
@@ -21,21 +20,71 @@
     $TelegramServiceConfiguration = $acm->getConfiguration('TelegramService');
     define("TELEGRAM_BOT_NAME", $TelegramServiceConfiguration['BotName'], false);
 
-    $CoffeeHouse = new CoffeeHouse();
-    $telegram = new Longman\TelegramBot\Telegram(
-        $TelegramServiceConfiguration['BotToken'],
-        $TelegramServiceConfiguration['BotName']
-    );
-    $telegram->addCommandsPaths([__DIR__ . DIRECTORY_SEPARATOR . 'commands']);
+    if(strtolower($TelegramServiceConfiguration['BotName']) == 'true')
+    {
+        define("TELEGRAM_BOT_ENABLED", true);
+    }
+    else
+    {
+        define("TELEGRAM_BOT_ENABLED", false);
+    }
+
+    // Update Webhook
+    $set_webhook = false;
+    if(isset($_GET['set_webhook']))
+    {
+        if($_GET['set_webhook'] == '1')
+        {
+            $set_webhook = true;
+        }
+    }
 
     try
     {
-        $telegram->handle();
+        $telegram = new Longman\TelegramBot\Telegram(
+            $TelegramServiceConfiguration['BotToken'],
+            $TelegramServiceConfiguration['BotName']
+        );
+        $telegram->addCommandsPaths([__DIR__ . DIRECTORY_SEPARATOR . 'commands']);
     }
-    catch (TelegramException $e)
+    catch (Longman\TelegramBot\Exception\TelegramException $e)
     {
         ?>
-        <h1>Access Denied</h1>
-        <p>Nothing to see here, the current time is <?PHP print(hash('sha256', time() . 'IV')); ?></p>
+        <h1>Error</h1>
+        <p>Something went wrong here, try again later</p>
         <?php
+    }
+
+
+    if($set_webhook == true)
+    {
+        try
+        {
+            $result = $telegram->setWebhook($TelegramServiceConfiguration['WebHook']);
+            if ($result->isOk())
+            {
+                echo $result->getDescription();
+            }
+        }
+        catch (Longman\TelegramBot\Exception\TelegramException $e)
+        {
+            ?>
+            <h1>Error</h1>
+            <p>Something went wrong here, try again later</p>
+            <?php
+        }
+    }
+    else
+    {
+        try
+        {
+            $telegram->handle();
+        }
+        catch (TelegramException $e)
+        {
+            ?>
+            <h1>Access Denied</h1>
+            <p>Nothing to see here, the current time is <?PHP print(hash('sha256', time() . 'IV')); ?></p>
+            <?php
+        }
     }
